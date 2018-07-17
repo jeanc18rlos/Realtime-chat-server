258175430edc6d1aa454c0be409ddf78c07aaf29
jeanc18rlos <jeanc16rlos@gmail.com>
2018-07-16 20:58:55 -0400

    first commit

--- config/main.js
+++ config/main.js
@@ -2,9 +2,9 @@ module.exports = {
  // Setting port for the server
     'port' : process.env.PORT || 3000,
 // Secret key for JWT signing and encryption
-    'secret': 'jeanROjas',
+    'secret': 'mysecret',
 // Database connection information
-    'database': 'mongodb://jeanc16rlos:PAris1515@ds135441.mlab.com:35441/pokersaas',
+    'database': 'mongopath',
     
 
 }
